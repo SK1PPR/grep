8849f524cb979ed9ff75cb016efed898360cfb4a
SK1PPR <agrawalkhushal04@gmail.com>
2024-08-07 09:32:41 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
         return input_line.chars().any(char::is_numeric);
     } else if pattern == r"\w" {
         return input_line.chars().any(char::is_alphanumeric);
-    } else if pattern.chars().nth(0).unwrap() == '[' {
+    } else if pattern.starts_with('[') && pattern.ends_with(']') {
+        if pattern.chars().nth(1).unwrap() == '^' {
+            return pattern.chars().any(|c| c != '[' && c != '^' && c != ']' && !input_line.contains(c))
+        }
         return pattern.chars().any(|c| input_line.contains(c));
     } else if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
@@ -18,7 +21,6 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
-
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
@@ -28,7 +30,6 @@ fn main() {
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
-
 
     if match_pattern(&input_line, &pattern) {
         process::exit(0)
